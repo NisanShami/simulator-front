dd60ebbc5f78d4c696b49237cf0c98e47a224e4c
Nisan Shamir <nisan.s@lsports.eu>
2024-05-23 10:08:38 +0300

    * fixtures table

--- src/app/components/fixture-table/fixture-table.component.ts
+++ src/app/components/fixture-table/fixture-table.component.ts
@@ -1,24 +1,24 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-// import { Table } from 'primeng/table';
-import { PrimengModule } from 'src/app/primeng-module/primeng/primeng.module';
+import { AfterViewInit, Component } from '@angular/core';
+import { PrimengModule } from '../../primeng-module/primeng/primeng.module';
 import { FixtureService } from '../services/fixture.service';
 import { Photo } from '../models/fixture.model';
+import { FixtureFiltersComponent } from '../fixture-filters/fixture-filters.component';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-fixture-table',
   templateUrl: './fixture-table.component.html',
   styleUrls: ['./fixture-table.component.scss'],
   standalone: true,
-  imports: [PrimengModule],
+  imports: [PrimengModule, FixtureFiltersComponent],
 })
 export class FixtureTableComponent implements AfterViewInit {
-  // @ViewChild(Table) tableRef!: Table;
 
   headerColsNames!: string[];
   headerCols!: string[];
   selectedPhotos!: any;
 
-  photos!: any[];
+  data!: any[];
 
   constructor(private fixtureService: FixtureService) {}
 
@@ -30,13 +30,16 @@ export class FixtureTableComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.fixtureService.getData().subscribe((res) => {
-      this.makeHeaderCols(res[0]);
-      this.photos = res;
-    });
-
-    setTimeout(() => {
-      console.log(this.selectedPhotos)
-    }, 5000);
+    this.fixtureService.filtersAsQueryString$
+      .pipe(
+        switchMap((r) => {
+          console.log(r);
+          return this.fixtureService.getAllData('fixtures', r);
+        })
+      )
+      .subscribe((res) => {
+        this.makeHeaderCols(res[0]);
+        this.data = res;
+      });
   }
 }
